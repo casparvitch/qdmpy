b4ae60fd286959309da2452be9b1dcc0cd21c2b3
Sam Scholten <samcaspar@gmail.com>
2021-01-27 10:38:33 +1100

    Starting on Bfield recon

---
+++ QDMPy/Bfield_calc.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+This module holds tools for calculating the magnetic field (\\vec{B}) from
+ODMR datasets (after they've been fit with the `QDMPy.fit_interface` tooling.
+
+Functions
+---------
+ - `QDMPy.Bfield_calc`
+"""
+
+# ============================================================================
+
+__author__ = "Sam Scholten"
+
+# ============================================================================
