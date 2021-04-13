fixed substantial bugs in fourier analysis. They were mostly just sign issues, but gave incorrect results for all but j_from_bxy.

src/qdmpy/constants.py
@@ -167,10 +167,10 @@ Measure x amps = x A
 
 # nv orientations (unit vectors) wrt lab frame [x, y, z]
 NV_AXES_100_110 = [
-    {"nv_number": 1, "ori": [np.sqrt(2 / 3), 0, np.sqrt(1 / 3)]},
-    {"nv_number": 2, "ori": [-np.sqrt(2 / 3), 0, np.sqrt(1 / 3)]},
-    {"nv_number": 3, "ori": [0, np.sqrt(2 / 3), -np.sqrt(1 / 3)]},
-    {"nv_number": 4, "ori": [0, -np.sqrt(2 / 3), -np.sqrt(1 / 3)]},
+    {"nv_number": 0, "ori": [np.sqrt(2 / 3), 0, np.sqrt(1 / 3)]},
+    {"nv_number": 1, "ori": [-np.sqrt(2 / 3), 0, np.sqrt(1 / 3)]},
+    {"nv_number": 2, "ori": [0, np.sqrt(2 / 3), -np.sqrt(1 / 3)]},
+    {"nv_number": 3, "ori": [0, -np.sqrt(2 / 3), -np.sqrt(1 / 3)]},
 ]
 """
 <100> top face oriented, <110> edge face oriented diamond (CVD).
@@ -191,10 +191,10 @@ Purple plane corresponds to top (or bottom) face of diamond, orange planes corre
 """
 
 NV_AXES_100_100 = [
-    {"nv_number": 1, "ori": [np.sqrt(1 / 3), np.sqrt(1 / 3), np.sqrt(1 / 3)]},
-    {"nv_number": 2, "ori": [-np.sqrt(1 / 3), -np.sqrt(1 / 3), np.sqrt(1 / 3)]},
-    {"nv_number": 3, "ori": [np.sqrt(1 / 3), -np.sqrt(1 / 3), -np.sqrt(1 / 3)]},
-    {"nv_number": 4, "ori": [-np.sqrt(1 / 3), np.sqrt(1 / 3), -np.sqrt(1 / 3)]},
+    {"nv_number": 0, "ori": [np.sqrt(1 / 3), np.sqrt(1 / 3), np.sqrt(1 / 3)]},
+    {"nv_number": 1, "ori": [-np.sqrt(1 / 3), -np.sqrt(1 / 3), np.sqrt(1 / 3)]},
+    {"nv_number": 2, "ori": [np.sqrt(1 / 3), -np.sqrt(1 / 3), -np.sqrt(1 / 3)]},
+    {"nv_number": 3, "ori": [-np.sqrt(1 / 3), np.sqrt(1 / 3), -np.sqrt(1 / 3)]},
 ]
 """
 <100> top face oriented, <100> edge face oriented diamond (HPHT).
@@ -215,10 +215,10 @@ Purple plane: top face of diamond, orange plane: edge faces.
 """
 
 NV_AXES_111 = [
-    {"nv_number": 1, "ori": [0, 0, 1]},
+    {"nv_number": 0, "ori": [0, 0, 1]},
+    {"nv_number": 1, "ori": [np.nan, np.nan, np.nan]},
     {"nv_number": 2, "ori": [np.nan, np.nan, np.nan]},
     {"nv_number": 3, "ori": [np.nan, np.nan, np.nan]},
-    {"nv_number": 4, "ori": [np.nan, np.nan, np.nan]},
 ]
 """
 <111> top face oriented.

src/qdmpy/field/_geom.py
@@ -40,7 +40,7 @@ def get_unvs(options):
     Returns
     -------
     unvs : np array
-        Shape: (4,3). Equivalent to uNV_Z for each NV.
+        Shape: (4,3). Equivalent to uNV_Z for each NV. (Sorted largest to smallest Bnv)
     """
 
     bias_x, bias_y, bias_z = get_B_bias(options)
@@ -78,8 +78,6 @@ def get_unvs(options):
         sorted_dict = sorted(nv_axes, key=lambda x: x["mag"], reverse=True)
 
         for idx in range(len(sorted_dict)):
-            # nv_ori[idx, :] = sorted_dict[idx]["ori"] # not required anywhere?
-            # nv_signs[idx] = sorted_dict[idx]["sign"]
             unvs[idx, :] = np.array(sorted_dict[idx]["ori"]) * sorted_dict[idx]["sign"]
 
     return unvs

src/qdmpy/fourier/_shared.py
@@ -105,7 +105,7 @@ def define_k_vectors(shape, pixel_size, k_vector_epsilon):
         ky, kx = np.meshgrid(ky_vec, kx_vec, indexing="ij")
 
     k = np.sqrt(ky ** 2 + kx ** 2)
-    return -ky, kx, k  # negative here to maintain correct image orientation (ky greater at top).
+    return ky, -kx, k  # negative here to maintain correct image orientation (ass. NV above source)
 
 
 # ============================================================================

src/qdmpy/fourier/interface.py
@@ -209,7 +209,7 @@ def get_reconstructed_bfield(
 
     $$ k \neq 0 $$
 
-    we have (analogously for y)
+    we have
 
     $$ (\hat{B}_x^{\rm recon}(x,y,z=z_{\rm NV}), \hat{B}_y^{\rm recon}(x,y,z=z_{\rm NV})) = \frac{-i}{k} (k_x, k_y) \hat{B}_z(x,y,,z=z_{\rm NV}) $$
 
@@ -242,10 +242,12 @@ def get_reconstructed_bfield(
 
     ky, kx, k = qdmpy.fourier._shared.define_k_vectors(fft_bx.shape, pixel_size, k_vector_epsilon)
 
-    bz2bx = -(1j / k) * kx
-    bz2by = -(1j / k) * ky
-    bx2bz = (1j / k) * kx
-    by2bz = (1j / k) * ky
+    # could chuck in an 'NV_above_or_below' sign here. 2020-04-13 swapped minus sign
+    # -> assumes NV above source
+    bz2bx = (1j / k) * kx
+    bz2by = (1j / k) * ky
+    bx2bz = -(1j / k) * kx
+    by2bz = -(1j / k) * ky
 
     bz2bx = qdmpy.fourier._shared.set_naninf_to_zero(bz2bx)
     bz2by = qdmpy.fourier._shared.set_naninf_to_zero(bz2by)
@@ -439,7 +441,7 @@ def get_j_from_bz(
 
     ky, kx, k = qdmpy.fourier._shared.define_k_vectors(fft_bz.shape, pixel_size, k_vector_epsilon)
 
-    # define transform
+    # define transformation
     bz_to_jx, bz_to_jy = define_current_transform([0, 0, 1], ky, kx, k, standoff)
 
     hanning_filt = qdmpy.fourier._shared.hanning_filter_kspace(
@@ -899,7 +901,7 @@ def define_current_transform(u_proj, ky, kx, k, standoff=None):
     ---------
     u_proj : array-like
         Shape: 3, the direction the magnetic field was measured on (projected onto).
-    ky kx, k : np arrays
+    ky, kx, k : np arrays
         Wavenumber meshgrids, k = sqrt( kx^2 + ky^2 )
 
     standoff : float or None, default : None
@@ -916,6 +918,7 @@ def define_current_transform(u_proj, ky, kx, k, standoff=None):
         https://doi.org/10.1103/PhysRevApplied.14.024076
         https://arxiv.org/abs/2005.06788
     """
+
     if standoff:
         exp_factor = np.exp(1 * k * standoff)
     else:
@@ -933,7 +936,7 @@ def define_current_transform(u_proj, ky, kx, k, standoff=None):
 
 
 def define_magnetisation_transformation(ky, kx, k, standoff):
-    """M => b fourier-space transformation. (NOTE given as inverse of direction 'wanted')
+    """M => b fourier-space transformation.
 
 
     Parameters