d63251e0e3fc88413e3a32245b1b90c1c559cf8b
Sam Scholten <samcaspar@gmail.com>
2023-11-29 23:24:41 +1100

    Revert "another bug"
    This reverts commit 81e11f17e8615100af293ff99610f5547153ea73.

--- src/qdmpy/pl/common.py
+++ src/qdmpy/pl/common.py
@@ -356,10 +356,11 @@ def bounds_from_range(options, param_key, guess):
             else:
                 raise RuntimeError("param range len should match guess len")
         # param guess and range are just single vals (easy!)
-        bounds = [
-            guess - rang,
-            guess + rang,
-        ]
+        else:
+            bounds = [
+                guess - rang,
+                guess + rang,
+            ]
     return bounds
 
 
